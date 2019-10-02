162622c037c85aa004258bc628a57e04dcc42a7f
AprilWJL <37832640+AprilWJL@users.noreply.github.com>
2019-10-02 16:06:13 +0800

    平面坐标转圆形坐标
    解决斜着跑速度为1.414的问题

--- Assets/ActorController.cs
+++ Assets/ActorController.cs
@@ -7,6 +7,7 @@ public class ActorController : MonoBehaviour {
     public GameObject model;
     public PlayerInput pi;
     public float walkSpeed = 1.5f;
+    public float runMultiplier = 2.7f;
 
     [SerializeField]
     private Animator anim;
@@ -22,17 +23,19 @@ public class ActorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        anim.SetFloat("forward", pi.Dmag);      //速度
+        float targetRunMulti = (pi.run ? 2.0f : 1.0f);
+        anim.SetFloat("forward", pi.Dmag * Mathf.Lerp(anim.GetFloat("forward"), targetRunMulti, 0.5f));     //动画控制
         if (pi.Dmag > 0.1f)
         {
-            model.transform.forward = pi.Dvec;      //方向
+            Vector3 targetForward = Vector3.Slerp(model.transform.forward, pi.Dvec, 0.4f);  //做差值，增加切换旋转动画时的平滑度
+            model.transform.forward = targetForward;      //旋转
         }
-        movingVec = pi.Dmag * model.transform.forward * walkSpeed;
+        movingVec = pi.Dmag * model.transform.forward * walkSpeed * (pi.run ? runMultiplier : 1.0f);
 	}
 
     private void FixedUpdate()
     {
         //rigid.position += movingVec * Time.fixedDeltaTime;
-        rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);
+        rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);   //用刚体来进行移动
     }
 }
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
 
+    [Header("===== Keys settings  =====")]
     public string keyUp = "w";
     public string keyDown = "s";
     public string keyLeft = "a";
     public string keyRight = "d";
 
+    public string keyA = "left shift";
+    public string keyB;
+    public string keyC;
+    public string keyD;
+
+    [Header("===== Output signals =====")]
     public float Dup;      //上下信号
     public float Dright;   //左右信号
-
     public float Dmag;      //速度
     public Vector3 Dvec;    //旋转
 
+    public bool run;        //跑步信号
+
+    [Header("===== Others =====")]
     public bool inputEnable = true;     //输入开关
 
     private float targetDup;
@@ -41,7 +50,29 @@ public class PlayerInput : MonoBehaviour {
 
         Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
-        Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
-        Dvec = Dright * transform.right + Dup * transform.forward;
+
+        Vector2 tempDAxis = SquareToCircle(new Vector2(Dright, Dup));
+        float Dright2 = tempDAxis.x;
+        float Dup2 = tempDAxis.y;
+
+
+        Dmag = Mathf.Sqrt((Dup2 * Dup2) + (Dright2 * Dright2));
+        Dvec = Dright2 * transform.right + Dup2 * transform.forward;
+
+        run = Input.GetKey(keyA);
+    }
+
+    /// <summary>
+    /// 平面坐标转圆形坐标
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    private Vector2 SquareToCircle(Vector2 input)
+    {
+        Vector2 output = Vector2.zero;
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2.0f);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2.0f);
+
+        return output;
     }
 }
