e5c98b9704965d43bb036e7963edfbb5f0efaf0f
AprilWJL <37832640+AprilWJL@users.noreply.github.com>
2019-10-05 16:44:08 +0800

    新增翻滚动画

--- Assets/ActorController.cs
+++ Assets/ActorController.cs
@@ -6,9 +6,10 @@ public class ActorController : MonoBehaviour {
 
     public GameObject model;
     public PlayerInput pi;
-    public float walkSpeed = 1.5f;
+    public float walkSpeed = 2.4f;
     public float runMultiplier = 2.7f;
     public float jumpVelocity = 2.0f;   //向上跳跃速度
+    public float rollVelocity = 1.0f;   //向上翻滚速度
 
     [SerializeField]
     private Animator anim;
@@ -29,6 +30,12 @@ public class ActorController : MonoBehaviour {
 	void Update () {
         float targetRunMulti = (pi.run ? 2.0f : 1.0f);
         anim.SetFloat("forward", pi.Dmag * Mathf.Lerp(anim.GetFloat("forward"), targetRunMulti, 0.5f));     //移动动画控制
+
+        if (rigid.velocity.magnitude > 0f)    //判断是否翻滚
+        {
+            anim.SetTrigger("roll");
+        }
+
         if (pi.jump)
         {
             anim.SetTrigger("jump");
@@ -58,18 +65,11 @@ public class ActorController : MonoBehaviour {
     /// </summary>
     public void OnJumpEnter()
     {
+        thrustVec = new Vector3(0, jumpVelocity, 0);       //新增跳跃冲量
         pi.inputEnable = false;     //跳跃时不能移动  
         lockPlanar = true;          //锁死平面移动
-        thrustVec = new Vector3(0, jumpVelocity, 0);       //新增跳跃冲量
         //print("onjumpenter");
     }
-
-    //public void OnJumpExit()
-    //{
-    //    pi.inputEnable = true;
-    //    lockPlanar = false;
-    //    //print("onjumpexit");
-    //}
 
     public void IsGround()
     {        
@@ -89,9 +89,14 @@ public class ActorController : MonoBehaviour {
 
     public void OnFallEnter()
     {
-        pi.inputEnable = true;
-        lockPlanar = false;
+        pi.inputEnable = false;
+        lockPlanar = true;
     }
 
-
+    public void OnRollEnter()
+    {
+        thrustVec = new Vector3(0, rollVelocity, 0);       
+        pi.inputEnable = false;     
+        lockPlanar = true;          //锁死平面移动
+    }
 }
